d8e9a3aa176951e775977f86bc55ea1c1cb795ff
Beznozhnikov Arseniy <apollonseven@Beznozhnikovs-MacBook-Pro.local>
2024-03-22 17:55:35 +0300

    fix: update host

--- client/src/shared/api/index.ts
+++ client/src/shared/api/index.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: `https://backend.dmstat.pro/` })
+const baseQuery = fetchBaseQuery({ baseUrl: `https://backend.gta5dm.pro/` })
 
 export const api = createApi({
     reducerPath: 'api',

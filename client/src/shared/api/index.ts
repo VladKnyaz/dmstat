06748e3b5ed8166707d85ddd24ee76fa5913cbc5
VladKnyaz <firekur@mail.ru>
2024-03-22 20:29:06 +0300

    fix: timer servers

--- client/src/shared/api/index.ts
+++ client/src/shared/api/index.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: `http://localhost:3000/` })
+const baseQuery = fetchBaseQuery({ baseUrl: `https://backend.gta5dm.pro/` })
 
 export const api = createApi({
     reducerPath: 'api',

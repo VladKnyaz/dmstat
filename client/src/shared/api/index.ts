5721c2124650c519ae97816e4cf9e9969e682890
VladKnyaz <firekur@mail.ru>
2024-03-29 14:53:54 +0300

    fix: api

--- client/src/shared/api/index.ts
+++ client/src/shared/api/index.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: `http://localhost:3000/` })
-// const baseQuery = fetchBaseQuery({ baseUrl: `https://backend.gta5dm.pro/` })
+// const baseQuery = fetchBaseQuery({ baseUrl: `http://localhost:3000/` })
+const baseQuery = fetchBaseQuery({ baseUrl: `https://backend.gta5dm.pro/` })
 
 export const api = createApi({
     reducerPath: 'api',

d30a642bbc3ce07f248ab75af7614964f0bf5f55
VladKnyaz <firekur@mail.ru>
2024-04-05 16:31:42 +0300

    add auth

--- client/src/shared/api/index.ts
+++ client/src/shared/api/index.ts
@@ -3,14 +3,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
-// console.log(cookies.get('myCat')); // Pacman
-// cookies.set('fdsfds', 'dasda', {
-// })
 
-// console.log(cookies.get('23423')); // Pacman
+const url = 'https://backend.gta5dm.pro/'
+// const url = 'http://localhost:3000/'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: `http://localhost:3000/`,
+    baseUrl: url,
     prepareHeaders: (headers, getState) => {
         let token = cookies.get('token');
 
@@ -25,7 +23,7 @@ const baseQuery = fetchBaseQuery({
         return response.json()
     },
 })
-// const baseQuery = fetchBaseQuery({ baseUrl: `https://backend.gta5dm.pro/` })
+
 
 export const api = createApi({
     reducerPath: 'api',
